d8c9f41b8f0a9fac53b5db67ff63ddef0b9f52f6
jasper.alkema@icloud.com <jasper.alkema@icloud.com>
2025-04-14 00:18:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Country Filter
+
+Very simple Tampermonkey script that adds a Filter By Netherlands button to the leaderboard page of [ScoreSaber.com](https://scoresaber.com/)
+
+# Getting Started
+
+Get Tampermonkey for [Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) or [Firefox](https://addons.mozilla.org/firefox/addon/tampermonkey/). Then install the script from [here](https://github.com/fqrb/CountryFilter/main.js).
+
+# Usage
+
+Once it works, whenever you load a leaderboard page that is not already filtered by NL it should automatically add a button that will filter by the Netherlands on the right side.
+
+# Contact
+
+fqrb (Discord)
---
+++ main.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         Country Filter At Leaderboard
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Adds a button that lets you filter by your country to the leaderboard page
+// @author       fqrb
+// @match        https://scoresaber.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=scoresaber.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  let lastUrl = location.href;
+
+  function addCountryButton() {
+    const filterContainer = document.querySelector(".filter-container");
+
+    const url = new URL(window.location.href);
+
+    // We only want to add the button to the leaderboard page and if we are not already filtered by NL
+    if (
+      !url.pathname.startsWith("/leaderboard/") ||
+      url.searchParams.get("countries") === "NL"
+    ) {
+      console.log("Not allowed to add button here");
+      return;
+    }
+
+    // If the container exists we create the element
+    if (!filterContainer) {
+      return;
+    }
+
+    // Check if it already exists so we do not create multiple buttons
+    const existingButton = document.querySelector(".country-filter");
+    if (existingButton) return;
+
+    const countryButton = document.createElement("div");
+    countryButton.innerHTML = "Filter By Netherlands 🇳🇱";
+    countryButton.className = "filter country-filter svelte-1e5nn4d";
+    countryButton.onclick = function () {
+      url.searchParams.set("countries", "NL");
+      url.searchParams.set("page", "1");
+      window.location.href = url.toString();
+    };
+    filterContainer.appendChild(countryButton);
+    console.log("Added country button!");
+  }
+
+  // Polls every 500 ms to see if the url has changed and if so, adds the country button
+  setInterval(() => {
+    if (location.href !== lastUrl) {
+      lastUrl = location.href;
+      setTimeout(addCountryButton, 300); // Give DOM a moment to update
+    }
+  }, 500);
+
+  // Initial run in case you immediately navigate to a leaderboard page
+  // Wait for 500 ms before adding button to ensure all the elements have been added to the DOM tree
+  setTimeout(addCountryButton, 500);
+})();
